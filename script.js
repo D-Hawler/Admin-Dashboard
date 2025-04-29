96e246cdf47cdac6bcdd97927f027c09f3062933
Dante Hawler <202559257+D-Hawler@users.noreply.github.com>
2025-04-29 03:26:39 +0300

    pre-final
    Enhance UI and functionality: Updated styles in CSS for improved layout and responsiveness, added new SVG icons for interaction buttons, and implemented dynamic card generation in JavaScript for better content management.

---
+++ script.js
@@ -0,0 +1,54 @@
+const cardSpace = document.getElementsByClassName("cardSpace")[0];
+
+for (i = 0; i < 41; i++) {
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    cardSpace.appendChild(card);
+
+    const textArea = document.createElement("a");
+    textArea.setAttribute("href", "#");
+
+    card.appendChild(textArea);
+
+    const h2 = document.createElement("h2");
+    h2.textContent = "Super Cool Project";
+
+    const p = document.createElement("p");
+    p.textContent = "Sed tempus ut lacus ut scelerisque. Suspendisse sollicitudin nibh erat, id facilisis felis accumsan nec.";
+
+    textArea.appendChild(h2);
+    textArea.appendChild(p);
+
+    const interactionMenu = document.createElement("div");
+    interactionMenu.classList.add("interactionMenu");
+
+    card.appendChild(interactionMenu);
+
+    const interactionButton = document.createElement("a");
+    interactionButton.setAttribute("href", "#");
+
+    const button = document.createElement("img");
+    button.setAttribute("src", "./icon/star-plus-outline.svg");
+
+    interactionButton.appendChild(button);
+    interactionMenu.appendChild(interactionButton);
+
+    const interactionButton2 = document.createElement("a");
+    interactionButton2.setAttribute("href", "#");
+
+    const button2 = document.createElement("img");
+    button2.setAttribute("src", "./icon/eye-plus-outline.svg");
+
+    interactionButton2.appendChild(button2);
+    interactionMenu.appendChild(interactionButton2);
+
+    const interactionButton3 = document.createElement("a");
+    interactionButton3.setAttribute("href", "#");
+
+    const button3 = document.createElement("img");
+    button3.setAttribute("src", "./icon/source-fork.svg");
+
+    interactionButton3.appendChild(button3);
+    interactionMenu.appendChild(interactionButton3);
+};
